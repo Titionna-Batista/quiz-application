09d48bf5bcbb1fd24c4584b9ec1c1121d91573a0
Titionna-Batista <battit@knights.ucf.edu>
2021-04-28 12:40:35 -0400

    Preparing Questions for Quiz App

---
+++ quizzit/components/Questions.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "What is a man?",
+    }
+]
+
+/* Possible Questions:
+Categories:
+
+- Theme Parks
+1) What is the tallest ride in Walt Disney World?
+2) True or False: The Marvel franchise is not allowed in Universal.
+3) What is the name of Disney's Shopping Center?
+4) What is the name of the tallest roller coaster in Universal Orlando Resort?
+5) What roller coaster is the fastest on record?
+
+- Monsters
+1) What monster was invented by Mary Shelley?
+2) Who was the first literary vampire?
+3) What is the general term for werewolfism?
+4) What movie recently featured a creature similar to the Creature from the Black Lagoon?
+5) What two monsters are heavily featured in the hit saga 'Twilight'?
+
+- State Capitals
+1) What is the capital of Massachussets?
+2) What is the capital of Washington State?
+3) What is the capital of Ohio?
+4) What is the capital of Alaska?
+5) What is the capital of Arkansas?
+
+- Random
+1) What is the largest lake in the world?
+2) What color is a Welsh Poppy?
+3) What is the oldest breed of dog?
+4) Which chess piece can only move diagonally?
+5) 
+
+*/
